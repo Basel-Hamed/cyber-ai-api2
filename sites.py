328ef9eb72438ecb3e56903803723ade082e2925
Basel-Hamed <scef.baselhamed.bd@gmail.com>
2026-03-12 16:57:37 +0600

    Add SITES dictionary with security resources

---
+++ sites.py
@@ -0,0 +1,26 @@
+SITES = {
+ "portswigger": {
+  "name": "PortSwigger",
+  "url": "https://portswigger.net/web-security"
+ },
+
+ "owasp": {
+  "name": "OWASP",
+  "url": "https://owasp.org/www-project-top-ten/"
+ },
+
+ "hackerone": {
+  "name": "HackerOne",
+  "url": "https://www.hackerone.com/blog"
+ },
+
+ "kali": {
+  "name": "Kali Linux Docs",
+  "url": "https://www.kali.org/docs/"
+ },
+
+ "tryhackme": {
+  "name": "TryHackMe",
+  "url": "https://tryhackme.com/resources/blog"
+ }
+}
