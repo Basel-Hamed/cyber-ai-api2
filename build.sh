7fe48ff1cb3d277757b0b858c2384c0d66734050
Basel-Hamed <scef.baselhamed.bd@gmail.com>
2026-03-13 06:30:09 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-echo "🚀 Build process started..."
-
-# Pip আপডেট
+echo "🚀 Build started..."
 python -m pip install --upgrade pip
-
-# Build dependencies
-python -m pip install --upgrade setuptools wheel
-
-# Install requirements
 pip install -r requirements.txt
-
-# NLTK data
-python -c "import nltk; nltk.download('punkt'); nltk.download('stopwords'); nltk.download('wordnet')"
-
 echo "✅ Build completed!"
